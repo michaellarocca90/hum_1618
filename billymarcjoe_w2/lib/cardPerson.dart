2a0e5bccd5b145fe4e72bdf5af0a2507d75f569a
shark-bit <marc.d.lewis21@gmail.com>
2019-03-25 20:47:15 -0700

    Hello

---
+++ billymarcjoe_w2/lib/cardPerson.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+class CardPerson {
+  
+  String name;
+
+  String img_url;
+
+  CardPerson(String name, String img_url) {
+    this.name = name;
+    this.img_url = img_url;
+}
+
+}
+
+class CardPersonManger {
+  int counter = 0;
+  List<CardPerson> cardPersons = new List();
+  List<CardPerson> createCardPersons () {
+
+    cardPersons.add(CardPerson("Jason", "Splash_1.jpg"));
+
+    cardPersons.add(CardPerson("Joe", "Splash_2.jpg"));
+
+    cardPersons.add(CardPerson("Billy", "Splash_3.jpg"));
+
+    return cardPersons;
+  }
+   void createNewCardPerson(){
+    cardPersons.add(CardPerson("New Guy"+counter.toString(),"Splash_4.jpg"));
+    counter++;
+  }
+}
---
+++ billymarcjoe_w2/lib/cardWidget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'cardPerson.dart';
+
+
+
+class CardWidget extends StatefulWidget{
+
+  @override
+  State createState(){
+    return CardWidgetState();
+  }
+}
+
+class CardWidgetState extends State<CardWidget>{
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      body: _buildCardWidget(),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNewCardWidget,
+
+        child:Icon(Icons.add),
+
+
+      ),
+
+
+    );
+
+  }
+
+  void _addNewCardWidget(){
+
+    manager.createNewCardPerson();
+
+
+
+    
+    setState(() {
+      
+    });
+
+  }
+
+  Widget _buildCardWidget(){
+
+    return ListView.builder(
+
+      itemBuilder: (context, i) {
+
+        final index = i % persons.length;
+
+        print(i);
+        return _buildCardItem(persons[index]);
+      }
+    );
+  }
+    static CardPersonManger manager = CardPersonManger();
+    List<CardPerson> persons = manager.createCardPersons();
+
+
+    Widget _buildCardItem(CardPerson person){
+
+      return Align(alignment: Alignment.center,
+        child:Column(
+          children: <Widget>[
+            Text(person.name),
+            Image.asset(person.img_url),
+          ],
+        ));
+
+
+    }
+
+}
