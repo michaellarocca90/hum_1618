4eb3c4b57aed0e30cd2dcb11f6a957d5b8911c96
michaellarocca90 <33819929+michaellarocca90@users.noreply.github.com>
2019-03-27 21:54:57 -0700

    Adds auth packages for android, Sets up route directories

---
+++ lib/auth.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:au'
+
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+final FirebaseAuth _auth = FirebaseAuth.instance;
