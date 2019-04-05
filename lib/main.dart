115d06988c3898e5c2582b7a8a508d3520348c24
michaellarocca90 <33819929+michaellarocca90@users.noreply.github.com>
2019-04-04 21:43:59 -0700

    Core widgets from master, login + signup styled

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,15 @@
 import 'package:flutter/material.dart';
-import 'auth.dart';
-import 'login_signup.dart';
+import './pages/splash.dart';
 
-// import 'package:hum_1618/guest_routes/home_page.dart';
+void main() => runApp(HumApp());
 
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HumApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginSignup(new Auth()),
-    );
+        title: 'Hum',
+        theme: ThemeData(primarySwatch: Colors.purple),
+        home: SplashPage());
   }
 }
