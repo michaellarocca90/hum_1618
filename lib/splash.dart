a602181c4f71adbb7cbf709dffb9b6a6e54ec692
michaellarocca90 <33819929+michaellarocca90@users.noreply.github.com>
2019-04-04 20:13:14 -0700

    finishes splash and adds assets

--- lib/splash.dart
+++ lib/splash.dart
@@ -4,7 +4,7 @@ import 'base_background.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BaseBackground(_splashButtons(), "assets/hum_shake_logo.jpg");
+    return BaseBackground(_splashButtons(), "assets/hum_shake_purp_middle.png");
   }
 }
 
@@ -15,8 +15,9 @@ Widget _splashButtons() {
 // this can be refactored to a single function that takes an alignment property
 Widget _registerButton(){
   return Container(
-      alignment: Alignment(-0.75, -0.5),
+      alignment: Alignment(-0.75, 0.85),
       child: MaterialButton(
+        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
         onPressed: () => {},
         child: new Text(
           'Register',
@@ -24,25 +25,26 @@ Widget _registerButton(){
             fontSize: 17.5, 
           ),
         ),
-        color: Colors.purple,
+        color: Colors.white,
         splashColor: Colors.purpleAccent,
-        textColor: Colors.white,
+        textColor: Colors.purple,
       ));
 }
 
 Widget _signInButton(){
   return Container(
-      alignment: Alignment(0.75, -0.5),
+      alignment: Alignment(0.75, 0.85),
       child: MaterialButton(
         onPressed: () => {},
+        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
         child: new Text(
           'Sign-in',
           style: new TextStyle(
             fontSize: 17.5, 
           ),
         ),
-        color: Colors.purple,
+        color: Colors.white,
         splashColor: Colors.purpleAccent,
-        textColor: Colors.white,
+        textColor: Colors.purple,
       ));
 }
